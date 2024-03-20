2b598cd1106aacbfd0ca9df34c490a450e4674b1
rodmen <rodrigo.gasolero@gmail.com>
2024-03-20 20:28:29 -0300

    imganesmock

--- asyncMock.js
+++ asyncMock.js
@@ -5,7 +5,7 @@ const products = [
       name: 'Camiseta NBA Lakers',
       description: 'Camiseta oficial de los Lakers de Los Ángeles.',
       price: 40,
-      image: '/img/lakers.jpg',
+      image: '/public/lakers.jpg',
     },
     {
       id: 2,
@@ -13,7 +13,7 @@ const products = [
       name: 'Camiseta NBA Bulls',
       description: 'Camiseta oficial de los Bulls de Chicago.',
       price: 35,
-      image: '/img/bulls.jpg',
+      image: '/public/bulls.jpg',
     },
     {
       id: 3,
@@ -21,7 +21,7 @@ const products = [
       name: 'Camiseta NBA Warriors',
       description: 'Camiseta oficial de los Warriors de Golden State.',
       price: 38,
-      image: '/img/warriors.jpg',
+      image: '/public/warriors.jpg',
     },
     {
       id: 4,
@@ -29,7 +29,7 @@ const products = [
       name: 'Jersey NFL Patriots',
       description: 'Jersey oficial de los New England Patriots.',
       price: 50,
-      image: '/img/patriots.jpg',
+      image: '/public/patriots.jpg',
     },
     {
       id: 5,
@@ -37,7 +37,7 @@ const products = [
       name: 'Jersey NFL Cowboys',
       description: 'Jersey oficial de los Dallas Cowboys.',
       price: 45,
-      image: '/img/cowboys.jpg',
+      image: '/public/cowboys.jpg',
     },
     {
       id: 6,
@@ -45,7 +45,7 @@ const products = [
       name: 'Jersey NFL Packers',
       description: 'Jersey oficial de los Green Bay Packers.',
       price: 48,
-      image: '/img/packers.jpg',
+      image: '/public/packers.jpg',
     },
   ];
   
